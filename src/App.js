4234ad9e4f39d803cb30c6fe3b709ab15c63b159
nini-max28 <stephanie_28_doucet@hotmail.com>
2025-09-28 09:11:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   // TOUS LES ÉTATS
-const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'https://backend-1-ohz7.onrender.com;
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'https://backend-1-ohz7.onrender.com';
   console.log('API_BASE_URL utilisée:', API_BASE_URL);
 
   const [backendConnected, setBackendConnected] = useState(false);
